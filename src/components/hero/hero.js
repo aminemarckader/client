0940ec52619d0679d84aed6dd65cdbbc6be57ce8
Amine Kader <aminemarckader@gmail.com>
2021-06-17 19:46:58 +0100

    change hero picture

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,13 +6,13 @@ class Hero extends Component {
   <div className="container d-flex h-100">
     <div className="row justify-content-center align-self-center" data-aos="fade-up">
       <div className="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay={100}>
-        <h2>We believe that risk assessment is the core of safety</h2>
+        <h2>We believe risk assessment is the core of safety</h2>
         <div>
-          <a href="#about" className="btn-get-started scrollto">Get Started</a>
+          <a href="#about" className="btn-get-started scrollto">Start Here</a>
         </div>
       </div>
       <div className="col-lg-6 intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay={200}>
-        <img src="assets/img/intro-img.svg" alt="intro" className="img-fluid" />
+        <img src="assets/img/intro.svg" alt="intro" className="img-fluid" />
       </div>
     </div>
   </div>
