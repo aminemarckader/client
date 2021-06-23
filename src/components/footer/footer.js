214f76c3883634c5f60e89f01a1af8a9c9d466ac
Amine Kader <aminemarckader@gmail.com>
2021-06-23 21:06:18 +0100

    fix scroll to top bug

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -29,7 +29,7 @@ class Footer extends Component {
               <div className="footer-links">
                 <h4>Useful Links</h4>
                 <ul>
-                  <li><HashLink className="scrollto" smooth to="#">Home</HashLink></li>
+                  <li><HashLink smooth to="#">Home</HashLink></li>
                   <li><HashLink className="scrollto" smooth to="#about">About</HashLink></li>
                   <li><HashLink className="scrollto" smooth to="#services">Services</HashLink></li>
                   <li><Link to="/terms" >Terms of service</Link></li>
