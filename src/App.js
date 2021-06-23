db32ea517db12b0e4d19d69709e3b499a8acec35
Amine Kader <aminemarckader@gmail.com>
2021-06-23 23:52:08 +0100

    remove en.json

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ i18n
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
     resources: {
       french,
-      english
+
     },
      // if you're using a language detector, do not define the lng option
     fallbackLng: "fr",
