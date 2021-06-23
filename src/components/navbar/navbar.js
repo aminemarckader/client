6e36451ff6f81a0c5570426f8bc3e8104e74f34f
Amine Kader <aminemarckader@gmail.com>
2021-06-23 21:00:17 +0100

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { HashLink } from 'react-router-hash-link';
 
 class Navbar extends Component {
   render() {
     return <nav id="navbar" className="navbar order-last order-lg-0">
     <ul>
-      <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-      <li><a className="nav-link scrollto" href="#about">About</a></li>
-      <li><a className="nav-link scrollto" href="#services">Services</a></li>
+      <li><HashLink className="nav-link scrollto active" to="#hero">Home</HashLink></li>
+      <li><HashLink className="nav-link scrollto" to="#about">About</HashLink></li>
+      <li><HashLink className="nav-link scrollto" to="#services">Services</HashLink></li>
       {/* <li><a className="nav-link scrollto " href="#portfolio">Portfolio</a></li>
       <li><a className="nav-link scrollto" href="#team">Team</a></li> */}
       {/* <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down" /></a>
@@ -26,7 +27,7 @@ class Navbar extends Component {
           <li><a href="#">Drop Down 4</a></li>
         </ul>
       </li> */}
-      <li><a className="nav-link scrollto" href="#footer">Contact</a></li>
+      <li><HashLink className="nav-link scrollto" to="#footer">Contact</HashLink></li>
     </ul>
     <i className="bi bi-list mobile-nav-toggle" />
   </nav>
