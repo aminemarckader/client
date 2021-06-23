085d8ece39ed8614953fda7dcd86248ab7b127ec
Amine Kader <aminemarckader@gmail.com>
2021-06-23 23:15:24 +0100

    add internationalization

--- src/App.js
+++ src/App.js
@@ -12,6 +12,56 @@ import {
   Switch,
   Route} from "react-router-dom";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: {
+          "Contact Us": "Contact Us",
+          "Send Message": "Send Message",
+          "Thank You": "Your message has been sent. Thank you!",
+          "Name":"Your Name",
+          "Email":"Your Email",
+          "Subject":"Subject",
+          "Message":"Your Message",
+          "Name required":"Your Name is required",
+          "Email required":"Your Email is required",
+          "Subject required":"Subject is required",
+          "Message required":"Your Message is required",
+        }
+      },
+      fr: {
+        translation: {
+          "Contact Us": "Contactez Nous",
+          "Send Message": "Envoyer Message",
+          "Thank You": "Votre message a ete bien envoye. Merci!",
+          "Name":"Votre Nom",
+          "Email":"Votre Email",
+          "Subject":"Votre Sujet",
+          "Message":"Votre Message",
+          "Name required":"Votre Nom est requis",
+          "Email required":"Votre Email est requis",
+          "Subject required":"Votre Suject est requis",
+          "Message required":"Votre Message est requis",
+        }
+      },
+    },
+    lng: document.querySelector('html').lang, // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
--- src/components/footer/contact.js
+++ src/components/footer/contact.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useToasts } from 'react-toast-notifications'
+import {useTranslation} from 'react-i18next';
 
 function Contact() {
+
   const { register, handleSubmit,formState: { errors } } = useForm();
   const { addToast } = useToasts();
+  const { t } = useTranslation();
   const onSubmit = (data) => {
     //alert(JSON.stringify(data));
-    addToast("Message sent successfully", {
+    addToast(t('Thank You'), {
       appearance: 'success',
       autoDismiss: true,
     })
@@ -22,10 +25,10 @@ function Contact() {
           name="username"
           className={`form-control ${errors.username ? 'is-invalid' : ''}`}
           id="name"
-          placeholder="Your Name*"
+          placeholder={t('Name')+'*'}
           {...register("username",{ required: true })}
         />
-		<div className="invalid-feedback">{errors.username && "First name is required"}</div>
+		<div className="invalid-feedback">{errors.username && t('Name required')}</div>
       </div>
       <div className="form-group mt-3">
         <input
@@ -33,7 +36,7 @@ function Contact() {
           className={`form-control ${errors.email ? 'is-invalid' : ''}`}
           name="email"
           id="email"
-          placeholder="Your Email*"
+          placeholder={t('Email')+'*'}
           {...register("email",{ required: true })}
         />
 		<span className="invalid-feedback">{errors.email && "your email is required"}</span>
@@ -44,7 +47,7 @@ function Contact() {
           className={`form-control ${errors.subject ? 'is-invalid' : ''}`}
           name="subject"
           id="subject"
-          placeholder="Subject*"
+          placeholder={t('Subject')+'*'}
           {...register("subject",{ required: true })}
         />
         	<span className="invalid-feedback">{errors.subject && "the subject is required"}</span>
@@ -54,21 +57,15 @@ function Contact() {
           className={`form-control ${errors.message ? 'is-invalid' : ''}`}
           name="message"
           rows={5}
-          placeholder="Message*"
+          placeholder={t('Message')+'*'}
           {...register("message",{ required: true })}
         />
         	<span className="invalid-feedback">{errors.message && "your message is required"}</span>
       </div>
-      <div className="my-3">
-        <div className="loading">Loading</div>
-        <div className="error-message" />
-        <div className="sent-message">
-          Your message has been sent. Thank you!
-        </div>
-      </div>
-      <div className="text-center">
-        <button type="submit" title="Send Message">
-          Send Message
+      
+      <div className="text-center my-3">
+        <button type="submit" title="{t('Send Message')}">
+          {t('Send Message')}
         </button>
       </div>
     </form>
