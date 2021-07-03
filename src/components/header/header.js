41f230704c8c48459c19f207d55ee3735c089f61
Amine Kader <aminemarckader@gmail.com>
2021-07-03 20:14:38 +0100

    changes in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -137,6 +137,25 @@ class Header extends Component {
   }, true)
 
   /**
+   * Mobile nav toggle
+   */
+   on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+  
+  /**
    * Scroll with ofset on page load with hash links in the url
    */
   window.addEventListener('load', () => {
